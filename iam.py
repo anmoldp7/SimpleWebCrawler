c4fa8b6d778a404149b941ecd041728cfef3d0bc
Anmol Deep <anmoldp7@gmail.com>
2018-08-26 03:23:22 +0530

    Python file for web crawling added.

---
+++ iam.py
@@ -0,0 +1,13 @@
+import urllib.request
+import re
+
+print("Enter page name")
+
+url = "https://en.wikipedia.org/wiki/"
+name = input()
+name = "_".join(name.title().split())
+url += name
+data = urllib.request.urlopen(url).read().decode("utf-8")
+
+m = re.search("last edited on ", data).start()
+print(data[m : m + 50].split('<')[0])
